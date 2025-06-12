64708409fd562ba37d6b6f0fe5a8d26e0f5f54b4
YericoPL99 <104123424+YPL4906@users.noreply.github.com>
2025-06-12 11:53:27 +0000

    Contador final

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -10,9 +10,32 @@ import '../styles/index.css'
 
 // components
 import Home from './components/Home';
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+//Numeros del contador
+let num1 = 0
+let num2 = 0
+let num3 = 0
+let num4 =0
+//Logica del funcionamiento
+setInterval(function(){
+    num1++;
+    if (num1 === 10) {
+      num1 = 0;
+      num2++;
+      if (num2 === 10) {
+        num2 = 0;
+        num3++;
+        if (num3 === 10) {
+          num3 = 0;
+          num4++;
+          if (num4 === 10) {
+            num4 = 0;
+          }
+        }
+      }
+    }
+      ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Home/>
+    <Home numero1={num1} numero2={num2} numero3={num3} numero4={num4}/>
   </React.StrictMode>,
 )
+}, 1000)
